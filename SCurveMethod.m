66426aff13497accc683ccbe4c1693a343570e37
Santeri <santeri.horttanainen@gmail.com>
2015-04-29 23:53:09 +0300

    interpit korjattu

---
+++ SCurveMethod.m
@@ -0,0 +1,64 @@
+% Paramaters
+N           = 780;
+MAXITER     = 20;
+beta        = .000001;
+epsilon     = 0.001;
+alphas      = 20;
+alphavec    = 14.^linspace(-6,4,alphas);
+loop        = length(alphavec(:));
+
+% Compute the simulated tomographic measurement data
+%[mncn measang target ] = A_NoCrimeData_comp(noiselevel, N, Nang);
+
+% load the tomographic measurement data and images of sawn walnuts
+load measurement sino im
+nut1 = imread('pahkina_1.tif');
+nut1 = nut1(:,:,1);
+nut2 = imread('pahkina_2.tif');
+nut2 = nut2(:,:,1);
+
+% fbp is the filtered back projection of the original nut provided by
+% Alexander Meaney.
+fbp = im;
+
+target = nut1(2:end-1,2:end-1);
+mncn = sino;
+mncn = mncn.';
+measang = -90+[0:9:171]; % Maybe this should be modified to 0:9:170 or 0:9:179 ??
+
+% Compute the average of nonzero coefficients in sawn walnuts
+nzcoefs1 = NonZeroCoefficients(nut1,epsilon);
+nzcoefs2 = NonZeroCoefficients(nut2,epsilon);
+nzcoefs  = (nzcoefs1+nzcoefs2)/2;
+
+% Matrix to store data from loop
+data = linspace(1,loop,loop);
+
+for iii = 1:loop
+
+% Compute the Total Variation regularization of mncn with current alpha
+[ recn alpha obj smallestObjValue ] = TotalVariationFunction(alphavec(iii), MAXITER, beta, mncn, measang, target);
+
+% Compute the average number of nonzero coefficients from the sawn walnuts. 
+nzrecn = NonZeroCoefficients(recn,epsilon);
+
+% Store the data
+data(iii) = nzrecn;
+
+end
+
+% Interpolate the data
+querypoints         = 14.^linspace(-6,4,400);
+interpolation       = interp1(alphavec,data,querypoints,'spline');
+interpolationAlpha  = interp1(alphavec, alphavec,querypoints,'spline');
+
+% Optimal alpha
+tmp = abs(interpolation-nzcoefs);
+[value index] = min(tmp);
+alpha = interpolationAlpha(index);
+
+% Last reconstruction
+[ recn alpha obj smallestObjValue ] = TotalVariationFunction(alpha, MAXITER, beta, mncn, measang, target);
+
+save SCurveMethod fbp interpolation interpolationAlpha recn target obj alpha nzcoefs smallestObjValue
+exit
---
+++ TotalVariationFunction.m
@@ -0,0 +1,96 @@
+function [ recn alpha obj smallestObjValue ] = TotalVariationFunction( alpha, MAXITER, beta, mncn, measang, target )
+% Here we use the Barzilai and Borwain optimization method
+% to find the minimum of the regularized penalty functional
+%
+%		1/2 (Af - m)^T (Af - m) + alpha*sum(sqrt((f_i-f_j)^2+beta))
+%
+% with the sum ranging over i and j indexing all horizontally
+% and vertically neighboring pixel pairs.
+%
+% The approximation results from the positive constant beta rounding the
+% non-differentiable corner of the absolute value function. This way the
+% penalty functional becomes differentiable and thus efficiently optimizable.
+%
+% The following routine must be precomputed: XRsparseA_NoCrimeData_comp.m.
+%
+% Jennifer Mueller and Samuli Siltanen, March 2014
+
+% linesearch ja iteraatiossa korjaus jotta ei pysähdyttäisi piikkiin!
+
+[N,tmp] = size(target);
+
+% Incomprehensible correction factor. It is related to the way Matlab
+% normalizes the output of iradon.m. The value is empirically found and
+% tested to work to reasonable accuracy.
+corxn = 40.7467*N/64;
+
+% Optimization routine
+obj    = zeros(MAXITER+1,1);     % We will monitor the value of the objective function
+fold   = zeros(size(target));    % Initial guess
+gold   = XR_aTV_fgrad(fold,mncn,measang,corxn,alpha,beta);
+obj(1) = XR_aTV_feval(fold,mncn,measang,alpha,beta);
+
+% Make the first iteration step. Theoretically, this step should satisfy
+% the Wolfe condition, see [J.Nocedal, Acta Numerica 1992].
+% We use a simple line search method to compute a steplength so the new
+% value is smaller than the initial guess.
+t = .0001;
+tvec = (10).^linspace(-1,-6);
+
+for zzz = 1:length(tvec)
+    fnew  = max(fold - tvec(zzz)*gold,0);
+    firstValue = obj(1);
+    newValue   = XR_aTV_feval(fnew,mncn,measang,alpha,beta);
+    if firstValue>newValue
+        t = tvec(zzz);
+        break;
+    end
+    
+end
+
+% Compute new iterate point fnew and gradient gnew at fnew
+fnew = max(fold - t*gold,0);
+gnew = XR_aTV_fgrad(fnew,mncn,measang,corxn,alpha,beta);
+
+% Iteration counter
+its = 1;
+
+% Record value of objective function at the new point
+OFf        = XR_aTV_feval(fnew,mncn,measang,alpha,beta);
+obj(its+1) = OFf;
+
+% Keep record of the smallest obj value so the iteration doestn stop at a
+% spike
+smallestObjValue    = 999999999999;
+recnAtTheSmall      = fnew;
+
+% Barzilai and Borwein iterative minimization routine
+while (its  < MAXITER)
+    its = its + 1;
+    
+    % Compute steplength alpha
+    fdiff   = fnew - fold;
+    gdiff   = gnew - gold;
+    steplen = (fdiff(:).'*fdiff(:))/(fdiff(:).'*gdiff(:));
+    
+    % Update points, gradients and objective function value
+    fold = fnew;
+    gold = gnew;
+    fnew = max(fnew - steplen*gnew,0);
+    gnew = XR_aTV_fgrad(fnew,mncn,measang,corxn,alpha,beta);
+    OFf  = XR_aTV_feval(fnew,mncn,measang,alpha,beta);
+    obj(its+1) = OFf;
+    format short e
+    % Monitor the run
+    disp(['Iteration ', num2str(its,'%4d'),', objective function value ',num2str(obj(its),'%.3e')])
+    if its>MAXITER/3
+        smallestObjValue    = min(smallestObjValue,OFf);
+        if smallestObjValue == OFf
+            recnAtTheSmall      = fnew;
+        end
+    end
+end   % Iteration while-loop
+recn = recnAtTheSmall;
+
+end
+
